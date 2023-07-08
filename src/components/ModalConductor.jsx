14fe081ddedf13baefa981b95124f7a79e8b779c
rsindhiyar <rakshith_sindhiyar@intuit.com>
2023-07-08 22:56:47 +0530

    feat: added add item feature

--- src/components/ModalConductor.jsx
+++ src/components/ModalConductor.jsx
@@ -3,15 +3,18 @@ import { useSelector } from 'react-redux'
 import { getModal } from '../reducers'
 import { MODAL_TYPES } from '../constants';
 import CategoryModal from './modals/CategoryModal';
+import ItemModal from './modals/ItemModal';
 
 const ModalConductor = () => {
   const modalDetails = useSelector(getModal);
   const { type = '' } = modalDetails;
-  const { CATEGORY } = MODAL_TYPES;
+  const { CATEGORY, ITEM } = MODAL_TYPES;
 
   switch (type) {
     case CATEGORY:
       return <CategoryModal modalDetails={modalDetails} />
+    case ITEM:
+      return <ItemModal modalDetails={modalDetails} />
     default:
       return;
   }
